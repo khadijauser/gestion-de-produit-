a98aba61d22fcf5034b694ad00de139b17c061bb
ENAA <khadijauser779@gmail.com>
2024-12-20 15:50:46 +0100

    creation de class et menu principale

---
+++ gestion.js
@@ -0,0 +1,46 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("welcome in our product management");
+panneauDeControle();
+
+function panneauDeControle() {
+  console.log("Panneau de Contrôle ; Merci de saisir votre choix :");
+  console.log("1: Ajouter.");
+  console.log("2: Afficher.");
+  console.log("3: Modifier.");
+  console.log("4: Suprimer.");
+  console.log("5: Quitter.");
+  rl.question("Entrez votre choix : ", (choix) => {
+    switch (choix) {
+      case "1":
+        console.log("Ajouter un nouveau produit :");
+        panneauDeControle();
+        break;
+      case "2":
+        console.log("Disponibilité  des produits :");
+        panneauDeControle();
+        break;
+      case "3":
+        console.log("Modification sur les infos des produits :");
+        panneauDeControle();
+        break;
+      case "4":
+        console.log("Supprimer le produit :");
+        panneauDeControle();
+        break;
+      case "5":
+        console.log("\n A très bien tôt !");
+        rl.close();
+        break;
+      default:
+        console.log("\nChoix invalide. veuillez essayez les options disponible  ");
+        panneauDeControle();
+        break;
+    }
+  });
+}
---
+++ product.js
@@ -0,0 +1,10 @@
+class Product{
+    constructor(name,description,quantity,price){
+        this.name=name
+        this.description=description
+        this.quantity=quantity
+        this.price=price
+    }
+
+}
+module.exports=Product;
