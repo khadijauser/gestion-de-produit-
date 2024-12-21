005d2e1a52cae8a2e3c495de3e475259ecf15680
khadijab <khadijauser779@gmail.com>
2024-12-21 14:18:38 +0100

    add id generator

--- product.js
+++ product.js
@@ -1,10 +1,10 @@
 class Product{
     constructor(name,description,quantity,price){
-        this.name=name
-        this.description=description
-        this.quantity=quantity
-        this.price=price
+        this.id = date.now();
+        this.name=name;
+        this.description=description;
+        this.quantity=quantity;
+        this.price=price;
     }
-
 }
 module.exports=Product;
